7d53701e301beb2b8b7ad13972bccf9a49875aca
MartinaPerozzi <perozzimartina@gmail.com>
2023-04-12 09:21:42 +0200

    +added slug

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,8 @@ Route::middleware('auth')
     ->prefix('/admin')
     ->name('admin.')
     ->group(function () {
-        Route::resource('projects', ProjectController::class);
+        Route::resource('projects', ProjectController::class)
+            ->parameters(['projects' => 'project:slug']);
     });
 
 
